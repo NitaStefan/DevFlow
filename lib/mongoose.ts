4db36556293bdf0c7e155521471da3a68743fe10
sn <scarsan62@gmail.com>
2024-11-27 12:53:29 +0200

    Implement Logging

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,5 +1,7 @@
 import mongoose, { Mongoose } from "mongoose";
 
+import logger from "./logger";
+
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined");
@@ -21,7 +23,10 @@ if (!cached) {
 }
 
 const dbConnect = async (): Promise<Mongoose> => {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    logger.info("Using existing mongoose connection");
+    return cached.conn;
+  }
 
   if (!cached.promise)
     cached.promise = mongoose
@@ -29,11 +34,11 @@ const dbConnect = async (): Promise<Mongoose> => {
         dbName: "devflow",
       })
       .then((result) => {
-        console.log("Connected to MongoDB");
+        logger.info("Connected to MongoDB");
         return result;
       })
       .catch((error) => {
-        console.error("Error connceting to MongoDB", error);
+        logger.error("Error connceting to MongoDB", error);
         throw error;
       });
 
